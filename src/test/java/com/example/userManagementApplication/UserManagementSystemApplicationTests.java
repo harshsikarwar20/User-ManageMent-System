5a6d9bbe10de98d4dcf397d2ac2b345a133965a5
harshsikarwar20 <harshsikarwar125@gmail.com>
2023-04-21 22:57:27 +0530

    Files

---
+++ src/test/java/com/example/userManagementApplication/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
